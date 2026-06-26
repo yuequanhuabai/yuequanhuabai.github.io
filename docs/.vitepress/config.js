a0c57b5f052d355a6347bcc7490b0a41045e2347
baiyingwaming <baiyingwaming@foxmail.com>
2026-06-26 15:06:13 +0800

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,10 @@
+export default {
+    title: "Ethon Dev Log",
+    description: "开发过程与概念理解记录",
+    themeConfig: {
+        nav: [
+            { text: "Dev Logs", link: "/dev/" },
+            { text: "Concepts", link: "/concepts/" }
+        ]
+    }
+}
